aa3e9b8b0083f5190d439db554dd3360bd317859
Jack Nutting <jacknutting@mac.com>
2014-06-17 09:55:46 +0200

    Initial Commit

---
+++ DifferentViews/TBTViewController.m
@@ -0,0 +1,29 @@
+//
+//  TBTViewController.m
+//  DifferentViews
+//
+//  Created by JN on 2014-6-17.
+//  Copyright (c) 2014 thoughtbot. All rights reserved.
+//
+
+#import "TBTViewController.h"
+
+@interface TBTViewController ()
+
+@end
+
+@implementation TBTViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
